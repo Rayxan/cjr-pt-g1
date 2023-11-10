cdbd11f577d3447b9480790962e5e8c1365b3beb
Raylan <raylanwork@gmail.com>
2023-11-10 13:39:39 -0300

    feat: Testando sessões de Usuário

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 const express = require('express');
 const session = require('express-session');
+const bodyParser = require('body-parser');
 
 const port = 3000;
 var path = require('path')
 const app = express();
 
-app.use(session({secret: 'a2jhsdha3shjk5dsa'}))
+var login = "admin";
+var password = "123456";
 
+app.use(session({secret: 'a2jhsdha3shjk5dsa'}))
+app.use(bodyParser.urlencoded({extended:true})) //Recuperar dados de login
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-app.use('/public', express.static(path.join(__dirname, 'public')));
-
+// app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.post('/',(req,res)=>{
-    res.render('index');
+    if(req.body.password == password && req.body.login == login){
+        //Logado com sucesso
+        req.session.login = login;
+        res.render('logado', {login: login});
+    }else{
+        res.render('index');
+    }
 })
 
 app.get('/', (req,res)=>{
-    res.render('index');
+    if(req.session.login){
+        res.render('logado', {login: login});
+        console.log('O usuário ' + req.session.login + ' está logado no sistema')
+    }else{
+        res.render('index');
+    }
 })
 
 app.listen(port,() =>{
